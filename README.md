6284a2cbc28117a21e67295b394bba39fcac53ef
Timothy Sutton <tim@synthist.net>
2014-03-21 15:36:08 -0400

    First version of the script, README

--- README.md
+++ README.md
@@ -1,4 +1,129 @@
-make-munki-profile-pkg
-======================
+## make_munki_profile_pkg
 
-Make Configuration Profiles into Munki items.
+Given a Configuration Profile as an argument, this script:
+- builds a flat package that installs it to a configurable path
+- creates a postinstall script to install the profile
+  - (optionally removing the .mobileconfig file after installation)
+- creates a pkginfo file for installation with Munki, complete with
+  an uninstall script
+- calls munkiimport to import the pkg into the repo
+
+Run with `-h` to see the full help.
+
+There are some additional options to tweak the pkg/Munki item name
+and package identifier, but currently no other options to configure
+the pkginfo keys you may wish to configure afterwards
+(minimum_os_version, requires, update_for, etc.).
+
+
+### Rationale
+
+OS X has a mechanism (the `profiles` utility) to install profiles given a Configuration Profile on disk. It can also remove a profile given either the path to the file, or the profile's identifier.
+
+Coupled with the fact that we can package these data and instructions and version the package, we have at least a basic mechanism to consider that a profile has been installed.
+
+The installation of the profile is done as a postinstall script within the package rather than a Munki `postinstall_script` so that the package is not limited to use with Munki. The removal of the package does make use of Munki's `uninstall_script` removal method, however.
+
+If you would rather have a mechanism that can "enforce" that a profile is installed, and with exactly the contents you would expect, this is not it. This relies solely on the installer package receipt to consider the profile as being installed.
+
+### Examples
+
+No options are required:
+
+```bash
+➜ ./make_munki_profile_pkg.py suppress_ml_icloud_asst.mobileconfig
+
+pkgbuild: Inferring bundle components from contents of /var/folders/8t/5trmslfj2cnd5gxkbmkbn5fj38qb2l/T/tmpsgtSN2
+pkgbuild: Adding top-level postinstall script
+pkgbuild: Wrote package to /var/folders/8t/5trmslfj2cnd5gxkbmkbn5fj38qb2l/T/tmpVNbN1Z/suppress_ml_icloud_asst-2014.03.21.pkg
+Copying suppress_ml_icloud_asst-2014.03.21.pkg to /Volumes/munki_repo/pkgs/profiles/suppress_ml_icloud_asst-2014.03.21.pkg...
+Saving pkginfo to /Volumes/munki_repo/pkgsinfo/profiles/suppress_ml_icloud_asst-2014.03.21.plist...
+```
+
+But, there are several you can set:
+
+```bash
+➜ ./make_munki_profile_pkg.py \
+    --format-name "Profile_%%filename" \
+    --installed-path /Library/MyGreatOrg/Profiles \
+    --version 10.8 \
+    --pkg-prefix org.my.great \
+    --delete-after-install \
+    --munki-repo-destination "defaults/profiles" \
+    suppress_ml_icloud_asst.mobileconfig
+
+pkgbuild: Inferring bundle components from contents of /var/folders/8t/5trmslfj2cnd5gxkbmkbn5fj38qb2l/T/tmpdhHNxn
+pkgbuild: Adding top-level postinstall script
+pkgbuild: Wrote package to /var/folders/8t/5trmslfj2cnd5gxkbmkbn5fj38qb2l/T/tmpw2N1dL/Profile_suppress_ml_icloud_asst-10.8.pkg
+Copying Profile_suppress_ml_icloud_asst-10.8.pkg to /Volumes/munki_repo/pkgs/defaults/profiles/Profile_suppress_ml_icloud_asst-10.8.pkg...
+Saving pkginfo to /Volumes/munki_repo/pkgsinfo/defaults/profiles/Profile_suppress_ml_icloud_asst-10.8.plist...
+```
+
+In the latter case, the package's postinstall script:
+
+```bash
+#!/bin/sh
+
+/usr/bin/profiles -I -F /Library/MyGreatOrg/Profiles/suppress_ml_icloud_asst.mobileconfig
+
+/bin/rm -f /Library/MyGreatOrg/Profiles/suppress_ml_icloud_asst.mobileconfig
+```
+
+
+... and the generated pkginfo:
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>autoremove</key>
+    <false/>
+    <key>catalogs</key>
+    <array>
+        <string>testing</string>
+    </array>
+    <key>description</key>
+    <string></string>
+    <key>display_name</key>
+    <string>Profile_suppress_ml_icloud_asst-10.8</string>
+    <key>installed_size</key>
+    <integer>4</integer>
+    <key>installer_item_hash</key>
+    <string>fb975facf67986ad3f71f3face884cc46301606f22ccfb6b50834509ba507215</string>
+    <key>installer_item_location</key>
+    <string>defaults/profiles/Profile_suppress_ml_icloud_asst-10.8.pkg</string>
+    <key>installer_item_size</key>
+    <integer>2</integer>
+    <key>minimum_os_version</key>
+    <string>10.5.0</string>
+    <key>name</key>
+    <string>Profile_suppress_ml_icloud_asst</string>
+    <key>receipts</key>
+    <array>
+        <dict>
+            <key>installed_size</key>
+            <integer>4</integer>
+            <key>packageid</key>
+            <string>org.my.great.Profile_suppress_ml_icloud_asst</string>
+            <key>version</key>
+            <string>10.8</string>
+        </dict>
+    </array>
+    <key>uninstall_method</key>
+    <string>uninstall_script</string>
+    <key>uninstall_script</key>
+    <string>#!/bin/sh
+
+/usr/bin/profiles -R -p suppress_ml_icloud_asst
+/bin/rm -f /Library/MyGreatOrg/Profiles/suppress_ml_icloud_asst.mobileconfig
+/usr/sbin/pkgutil --forget org.my.great.Profile_suppress_ml_icloud_asst
+</string>
+    <key>uninstallable</key>
+    <true/>
+    <key>version</key>
+    <string>10.8</string>
+</dict>
+</plist>
+```
+
---
+++ make_munki_profile_pkg.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+#
+# make_munki_profile_pkg.py
+#
+# Tim Sutton, 2014
+
+import optparse
+import os
+import plistlib
+import shutil
+import subprocess
+import sys
+import tempfile
+
+from string import Template
+from time import localtime
+from xml.parsers.expat import ExpatError
+
+default_name_format_string = "%%filename"
+default_installed_path = "/usr/local/share"
+default_pkg_prefix = "com.github.makemunkiprofilepkg"
+default_repo_destination = "profiles"
+
+def main():
+    usage = "%prog [options] path/to/mobileconfig/file"
+    o = optparse.OptionParser(usage=usage)
+    o.add_option("-f", "--format-name", default=default_name_format_string,
+        metavar="FORMAT-STRING",
+        help=("A format string specifying the desired pkginfo item name, which "
+              "may contain tokens that are substituted. Current tokens "
+              "supported are '%%%%filename' (name component of file's basename), "
+              "and '%%%%id' (profile's PayloadIdentifier key). "
+              "Defaults to '%s'." % default_name_format_string))
+    o.add_option("-p", "--installed-path", default=default_installed_path,
+        help=("Installed path for the profile. Defaults to '%s'. "
+            % default_installed_path))
+    o.add_option("--pkg-prefix", default=default_pkg_prefix,
+        help=("Installer pkg identifier prefix. Defaults to '%s'. "
+              % default_pkg_prefix))
+    o.add_option("--version",
+        help=("Version of the built pkg. Defaults to 'YYYY.MM.DD' "
+              "derived from today's date."))
+    o.add_option("--delete-after-install", action="store_true",
+        default=False,
+        help=("Configure postinstall script to remove mobileconfig file "
+              "after installation."))
+    o.add_option("-d", "--munki-repo-destination", default=default_repo_destination,
+        help=("Destination directory in Munki repo. Defaults to '%s'. "
+              % default_repo_destination))
+
+    opts, args = o.parse_args()
+
+    if len(args) < 1:
+        o.print_help()
+        sys.exit(1)
+    if not opts.installed_path.startswith("/"):
+        print >> sys.stderr, (
+            "WARNING: Omitted leading slash for --installed-path %s, "
+            "automatically adding one." % opts.installed_path)
+        opts.installed_path = "/" + opts.installed_path
+
+    profile_path = args[0]
+    pkgbuild = "/usr/bin/pkgbuild"
+    munkiimport = "/usr/local/munki/munkiimport"
+    for executable in [pkgbuild, munkiimport]:
+        if not os.path.isfile(executable) or not os.access(executable, os.X_OK):
+            sys.exit("A required exeuctable, %s could not be found "
+                     "or is not executable!" % executable)
+
+    # Grab the profile's identifier for use later in the pkginfo's uninstall_script
+    try:
+        pdata = plistlib.readPlist(profile_path)
+        profile_identifier = pdata["PayloadIdentifier"]
+    except KeyError:
+        sys.exit("Expected 'PayloadIdentifier' key in profile, but none found!")
+    except ExpatError as e:
+        print >> sys.stderr, (
+            "Profile is either malformed or signed. Currently only unsigned "
+            "profiles are supported.")
+        sys.exit("Error: %s" % e.message)
+
+    # Version
+    version = opts.version
+    if not version:
+        now = localtime()
+        version = "%04d.%02d.%02d" % (now.tm_year, now.tm_mon, now.tm_mday)
+
+    # Naming of pkginfo item
+    profile_name = os.path.basename(profile_path).split(".mobileconfig")[0]
+    name_template = Template(opts.format_name.replace("%%", "$"))
+    templatables = {
+        "filename": profile_name,
+        "id": profile_identifier
+    }
+    item_name = name_template.safe_substitute(templatables)
+
+    # Installer package-related
+    pkg_filename = "%s-%s.pkg" % (item_name, version)
+    pkg_identifier = "%s.%s" % (opts.pkg_prefix, item_name)
+    pkg_output_path = os.path.join(tempfile.mkdtemp(), pkg_filename)
+
+    # -- payload
+    root = tempfile.mkdtemp()
+    pkg_payload_destination = os.path.join(root, opts.installed_path.lstrip("/"))
+    profile_installed_path = os.path.join(
+        opts.installed_path, os.path.basename(profile_path))
+    os.makedirs(pkg_payload_destination)
+    shutil.copy(profile_path, pkg_payload_destination)
+
+    # -- postinstall script
+    script_root = tempfile.mkdtemp()
+    script_path = os.path.join(script_root, "postinstall")
+    install_script = """#!/bin/sh
+
+/usr/bin/profiles -I -F %s
+""" % profile_installed_path
+    if opts.delete_after_install:
+        install_script += "\n/bin/rm -f %s" % profile_installed_path
+    with open(script_path, "w") as fd:
+        fd.write(install_script)
+    os.chmod(script_path, 0755)
+
+    # -- build it
+    subprocess.call([
+        pkgbuild,
+        "--root", root,
+        "--identifier", pkg_identifier,
+        "--version", version,
+        "--scripts", script_root,
+        pkg_output_path])
+
+    # Munki-related
+    # -- uninstaller script
+    uninstall_script_path = tempfile.mkstemp()[1]
+    uninstall_script = """#!/bin/sh
+
+/usr/bin/profiles -R -p %s
+/bin/rm -f %s
+/usr/sbin/pkgutil --forget %s
+""" % (profile_identifier, profile_installed_path, pkg_identifier)
+    with open(uninstall_script_path, "w") as fd:
+        fd.write(uninstall_script)
+
+    # -- import it
+    subprocess.call([
+        munkiimport,
+        "--nointeractive",
+        "--subdirectory", opts.munki_repo_destination,
+        "--uninstall-script", uninstall_script_path,
+        pkg_output_path
+        ]
+    )
+
+if __name__ == '__main__':
+    main()
